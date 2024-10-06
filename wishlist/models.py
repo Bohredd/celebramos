64766be2ed53ff9f53cbc6a38e94603ebcfe7691
Diogo <dapereira@inf.ufsm.br>
2024-10-06 18:14:00 -0300

    feat: project start e usuario

--- wishlist/models.py
+++ wishlist/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+class Site(models.Model):
+    nome = models.CharField(max_length=100)
+    url = models.URLField()
+    slug = models.SlugField(max_length=100, unique=True)
+    pago = models.BooleanField(default=False)
+    comprador = models.ForeignKey('usuarios.Usuario', on_delete=models.CASCADE)
+    lista = models.ForeignKey('Lista', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nome
+
+class Item(models.Model):
+    nome = models.CharField(max_length=100)
+    descricao = models.TextField()
+    site = models.ForeignKey(Site, on_delete=models.CASCADE)
+    comprado = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.nome
+
+class Lista(models.Model):
+    nome = models.CharField(max_length=100)
+    itens = models.ManyToManyField(Item)
+
+    def __str__(self):
+        return self.nome
