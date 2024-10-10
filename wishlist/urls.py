faae4201847f92c3732d42c51c8fbd8ca8f7c23d
Diogo <dapereira@inf.ufsm.br>
2024-10-10 12:40:28 -0300

    feat: start criar wish + home html

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -2,7 +2,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.homeview, name='home'), # em portugues
+    path('', views.homeview, name='home_pt'), # em portugues
     path('en/', views.homeview, name='home_en'), # em ingles
     path('cw/basico/', views.criar_site, name='wish_basica'),
     path('cw/profissional/', views.criar_site, name='wish_profissional'),
