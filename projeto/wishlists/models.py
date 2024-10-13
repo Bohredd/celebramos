3e6239a82d0b1f655392254e2d9094650f04ba31
Diogo <dapereira@inf.ufsm.br>
2024-10-13 01:03:44 -0300

    feat: models wishlists

--- projeto/wishlists/models.py
+++ projeto/wishlists/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Item(models.Model):
+    nome_item = models.CharField(max_length=100)
+    link_compra = models.URLField()
+    comprado = models.BooleanField(default=False)
+
+class Wishlist(models.Model):
+    nome_evento = models.CharField(max_length=100)
+    data_evento = models.DateField()
+    foi_paga = models.BooleanField(default=False)
+    valida = models.BooleanField(default=True)
+    criado_em = models.DateTimeField(auto_now_add=True)
+    creditos_colocados = models.DecimalField(max_digits=10, decimal_places=2)
+    link_musica = models.URLField()
