f6064b301749934797404d35621f14568269f61a
Diogo <dapereira@inf.ufsm.br>
2024-10-13 00:28:37 -0300

    feat: nova estrutura do projeto

--- usuarios/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Usuario, ConfiguracoesUsuario
-
-admin.site.register(Usuario)
-admin.site.register(ConfiguracoesUsuario)
--- wishlist/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Lista, Item, Site, TipoWishlist
-
-admin.site.register(Lista)
-admin.site.register(Item)
-admin.site.register(Site)
-admin.site.register(TipoWishlist)
