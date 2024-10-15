d7a70ba37e7fd3107de2cbbe85052e7f48defa59
Diogo <dapereira@inf.ufsm.br>
2024-10-15 10:39:25 -0300

    feat: ver wishlists + ver wishlists convidado + pagina inicial start

--- projeto/wishlists/views.py
+++ projeto/wishlists/views.py
@@ -1,23 +1,27 @@
 import base64
 import io
-
 import qrcode
 from django.contrib.auth.decorators import login_required
+from django.db.models import Sum
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
-
 from .forms import WishlistForm, ItemForm, EscolherPlanoForm
 from django.forms import modelformset_factory
 from django.contrib import messages
 from .models import Item, PlanoCredito, Wishlist
 from django.utils.safestring import mark_safe
-
 from usuarios.models import Usuario
 from decouple import config
 
 
 def pagina_inicial(request):
-    return render(request, 'wishlists/pagina_inicial.html')
+    quantia_wishlists = Wishlist.objects.count()
+    quantia_usuarios = Usuario.objects.count()
+    quantia_creditos = Usuario.objects.aggregate(quantia_creditos=Sum('creditos'))['quantia_creditos']
+
+    return render(request, 'wishlists/pagina_inicial.html',
+                  {'quantia_wishlists': quantia_wishlists, 'quantia_usuarios': quantia_usuarios,
+                   'quantia_creditos': quantia_creditos})
 
 
 @login_required
@@ -64,6 +68,7 @@ def criacao_wishlist(request):
     return render(request, 'wishlists/criacao_wishlist.html', {'form': form, 'formset': formset})
 
 
+@login_required
 def comprar_creditos(request):
     planos = PlanoCredito.objects.all()
 
@@ -88,10 +93,14 @@ def comprar_creditos(request):
         form = EscolherPlanoForm()
     return render(request, 'wishlists/comprar_creditos.html', {'planos': planos, 'form': form})
 
+
+@login_required
 def checkout_pagamento(request, plano_id):
     plano = PlanoCredito.objects.get(id=plano_id)
     return render(request, 'wishlists/checkout_pagamento.html', {'plano': plano})
 
+
+@login_required
 def compartilhar_wishlist(request, wishlist_id):
     wishlist = get_object_or_404(Wishlist, id=wishlist_id)
 
@@ -118,12 +127,29 @@ def compartilhar_wishlist(request, wishlist_id):
         'qr_code': img_base64,
     })
 
+
+@login_required
 def ver_wishlist_cliente(request, wishlist_id):
     wishlist = Wishlist.objects.get(id=wishlist_id)
-    itens = wishlist.itens.all()
-    return render(request, 'wishlists/ver_wishlist.html', {'wishlist': wishlist, 'itens': itens})
+    ItemFormSet = modelformset_factory(Item, form=ItemForm, extra=0)
+
+    print("Itens da wishlist:", wishlist.itens.all())
+
+    if request.method == 'POST':
+        formset = ItemFormSet(request.POST, queryset=wishlist.itens.all())
+        if formset.is_valid():
+            formset.save()
+            return redirect('ver_wishlist_cliente', wishlist_id=wishlist.id)
+    else:
+        formset = ItemFormSet(queryset=wishlist.itens.all())
+
+    return render(request, 'wishlists/ver_wishlist.html', {
+        'wishlist': wishlist,
+        'formset': formset,
+    })
+
 
 def ver_wishlist_convidado(request, wishlist_id):
     wishlist = Wishlist.objects.get(id=wishlist_id)
     itens = wishlist.itens.all()
-    return render(request, 'wishlists/ver_wishlist.html', {'wishlist': wishlist, 'itens': itens})
+    return render(request, 'wishlists/ver_wishlist_convidado.html', {'wishlist': wishlist, 'itens': itens})
